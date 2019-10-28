c2136f2e50a05f21f37f07247702d2770209996c
Gabriel Onike <onikegabriel@gmail.com>
2019-10-28 17:25:28 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -20,7 +20,7 @@ function pressIt(){
 }
 
 function submitIt(){
-  $('form').on('submit',function(){
+  $('form').on('Submit',function(){
     alert("Your form is going to be submitted now.");
   });
 }
